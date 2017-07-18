f55646941bd142234acd0e2a1403b565be042bc0
rajmytaj <chalasanidheeraj@gmail.com>
2017-07-18 20:37:18 +0000

    finished simple lookup script

---
+++ lookup_people.js
@@ -0,0 +1,27 @@
+var myArgs = process.argv.slice(2);
+
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  client.query("SELECT * FROM famous_people WHERE first_name = $1::text or last_name = $1::text", [myArgs[0]], (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    console.log('Searching...');
+    console.log(result.rows[0]);
+    client.end();
+  });
+});
